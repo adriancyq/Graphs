db378d597ac4ce32954fde811de559044543867b
adrian cordova y quiroz <adriancordovayquiroz@adrians-MacBook-Pro-3.local>
2017-03-02 15:28:26 -0800

    instream

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -22,7 +22,7 @@ int main(int argc, char ** argv) {
 
   // Check that user passed in 4 arguments
   if (argc != 5) {
-    cerr << "Wrong number of arguments."; << endl;
+    cerr << "Wrong number of arguments" << endl;
     return -1;
   }
 
@@ -40,8 +40,51 @@ int main(int argc, char ** argv) {
   graph->createGraph();
 
   // TODO Read in testPairs
+  //initialize the file stream
+  ifstream infile(testPairs);
+
+  bool have_header = false;
+
+  //keep reading linues until end of file is reached
+  while (infile){
+  	string s;
+
+  	//get the next line
+  	if (!getline( infile, s)) break;
+
+  	if (!have_header){
+  		have_header = true;
+  		continue;
+  	}
+
+  	// Parse the string 
+  	istringstream ss( s );
+  	vector <string> record;
+
+  	while (ss) {
+  		string next;
+
+  		//Get the next string before hitting a tab character
+  		//and put it in 'next'
+  		if (!getline( ss, next, '\t' )) break;
+
+  		record.push_back( next );
+  	}
+  	//We should have exactly 3 columns: name, movie, year
+  	if (record.size() != 3) {continue;}
+
+  	string actor_name(record[0]);
+  	string movie_title(record[1]);
+  	int movie_year = stoi(record[2]);
+
+  if (!infile.eof()) {
+        cerr << "Failed to read " << testPairs << "!\n";
+        return false;
+    }
+    infile.close();
 
   // TODO Use BFS to find links
   
   return 0;
+  }
 }
