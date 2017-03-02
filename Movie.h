a5c19ff198fbb3f86632f96c6db2b6ca2fbdeeed
adrian cordova y quiroz <adriancordovayquiroz@adrians-MacBook-Pro-3.local>
2017-03-02 13:52:06 -0800

    headers

--- Movie.h
+++ Movie.h
@@ -1,3 +1,10 @@
+/*
+ * Movie.h
+ * Author: Jonathan Chiu (A12113428), Adrian Cordova (A12010305)
+ * Date:   CSE 100 Winter 2017 2/28/17
+ *
+ * This file is meant to exist as a way to creates the movie objects.
+ */
 #ifndef MOVIE_H
 #define MOVIE_H
 
